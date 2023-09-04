62d92b8a6d98fa4b50460847303078bf950a6822
Raashid Naik <rx.naik@gmail.com>
2023-09-04 13:11:27 +0000

    Updated the rps bonus features

--- OO_RPS/rps_bonus_features.1.rb
+++ OO_RPS/rps_bonus_features.1.rb
@@ -1,5 +1,5 @@
 class Move
-  attr_accessor :choice, 
+  attr_accessor :choice, :name, :beats
 
   WINNING_MOVES = {  'rock' => ['scissors', 'lizard'],
                   'paper' => ['rock', 'spock'],
@@ -8,39 +8,29 @@ class Move
                   'spock' => ['rock', 'scissors'] }
 
   def initialize(choice)
-    @choice = Items.new(choice)
+    @name = choice
+    @beats = WINNING_MOVES[choice]
   end
 
   def >(other_move)
-    choice.beats.include?(other_move.choice.name)
+    beats.include?(other_move.name)
   end
 
   def <(other_move)
-    other_move.choice.beats.include?(choice.name)
+    other_move.beats.include?(name)
   end
 
   def to_s
-    choice.name
-  end
-end
-
-class Items
-  # This class will create a rock, paper, scissors, lizard or spock object
-  # depending on the player's selection.
-  attr_accessor :name, :beats
-
-  def initialize(value)
-    @name = value
-    @beats = Move::WINNING_MOVES[value]
+    name
   end
 end
 
 class Player
-  attr_accessor :move, :name, :move_hx, :freq_arr
+  attr_accessor :move, :name, :move_history, :freq_arr
 
   def initialize
     set_name
-    @move_hx = []
+    @move_history = []
   end
 end
 
@@ -60,37 +50,37 @@ class Human < Player
     choice = nil
     loop do
       puts "Please choose, rock, paper, scissors, spock, lizard"
-      choice = gets.chomp
+      choice = gets.chomp.downcase
       puts ""
       break if (Move::WINNING_MOVES.keys).include?(choice)
       puts "Sorry, invalid choice"
     end
     self.move = Move.new(choice)
-    move_hx << move.choice.name
+    move_history << move.name
   end
 end
 
 class Computer < Player
-  # The ROBOT_PREF hash assigns different selection personalities for each
+  # The COMPUTER_PREFERENCES hash assigns different selection personalities for each
   # computer name. The values in the array correspond to their preference for
   # [rock, paper, scissors, lizard, spock] respectively. For example
   # [1, 0, 0, 0, 0] will only select rock. The higher the value, the more
   # likely the item will be selected. Values must be >= 0 and the sum of all
   # values must be >= 1.
-  ROBOT_PREF = {  'R2D2' => [1, 1, 1, 1, 1], 'Hal' => [1, 0, 1, 0, 1],
+  COMPUTER_PREFERENCES = {  'R2D2' => [1, 1, 1, 1, 1], 'Hal' => [1, 0, 1, 0, 1],
                   'Chappie' => [0, 0, 0, 0, 1], 'Jonny5' => [4, 3, 2, 1, 0] }
   def set_name
-    self.name = ROBOT_PREF.keys.sample
+    self.name = COMPUTER_PREFERENCES.keys.sample
   end
 
   def choose
-    freq_arr = ROBOT_PREF[name]
-    comp_values = []
+    freq_arr = COMPUTER_PREFERENCES[name]
+    computer_values = []
     5.times do |idx|
-      (comp_values << ([Move::WINNING_MOVES.keys[idx]] * freq_arr[idx])).flatten!
+      (computer_values << ([Move::WINNING_MOVES.keys[idx]] * freq_arr[idx])).flatten!
     end
-    self.move = Move.new(comp_values.sample)
-    move_hx << move.choice.name
+    self.move = Move.new(computer_values.sample)
+    move_history << move.name
   end
 end
 
@@ -109,13 +99,16 @@ class Score
     puts ""
   end
 
-  def player_win(name)
-    puts "#{name} won!"
-    if Computer::ROBOT_PREF.keys.include?(name)
+  def record_win(name)
+    if Computer::COMPUTER_PREFERENCES.keys.include?(name)
       @computer += 1
     else
       @human += 1
     end
+  end
+
+  def display_win(name)
+    puts "#{name} won!"
   end
 
   def final_winner?
@@ -123,15 +116,7 @@ class Score
   end
 end
 
-class RPSGame
-  attr_accessor :human, :computer, :score
-
-  def initialize
-    @human = Human.new
-    @computer = Computer.new
-    @score = Score.new
-  end
-
+module Display
   def display_welcome_message
     puts "Welcome to Rock, Paper, Scissors, Spock, Lizard!"
     puts ""
@@ -141,41 +126,20 @@ class RPSGame
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
   end
-
-  def record_score(name)
-    score.player_win(name)
-  end
-
-  def display_winner
-    if human.move > computer.move
-      record_score(human.name)
-    elsif human.move < computer.move
-      record_score(computer.name)
-    else
-      puts "It's a tie!"
-    end
-    puts ""
-  end
-
-  def display_move_hx
+  
+  def display_move_history
     puts "Move History:"
     puts "#{human.name}:"
-    p human.move_hx
+    p human.move_history
     puts "#{computer.name}:"
-    p computer.move_hx
+    p computer.move_history
     puts ""
   end
-
-  def play_again?
-    answer = nil
-    loop do
-      puts "Would you like to play again? (y/n)"
-      answer = gets.chomp
-      break if ['y', 'n'].include?(answer.downcase)
-      puts "Sorry, must be y or n"
-    end
-    return false if answer.downcase == 'n'
-    return true if answer.downcase == 'y'
+  
+  def display_status
+    system 'clear'
+    score.display(human.name, computer.name)
+    display_move_history
   end
 
   def display_goodbye
@@ -187,11 +151,33 @@ class RPSGame
     puts ""
     puts "Thanks for playing Rock, Paper, Scissors, Spock, Lizard. Good bye!"
   end
+end
 
-  def display_status
-    system 'clear'
-    score.display(human.name, computer.name)
-    display_move_hx
+class RPSGame
+  attr_accessor :human, :computer, :score
+  
+  include Display
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+    @score = Score.new
+  end
+  
+  def find_winner
+    if human.move > computer.move
+      record_score(human.name)
+    elsif human.move < computer.move
+      record_score(computer.name)
+    else
+      puts "It's a tie!"
+    end
+    puts ""
+  end
+
+  def record_score(name)
+    score.record_win(name)
+    score.display_win(name)
   end
 
   def play
@@ -202,8 +188,9 @@ class RPSGame
       human.choose
       computer.choose
       display_moves
-      display_winner
-      break if score.final_winner? || !play_again?
+      find_winner
+      sleep(3)
+      break if score.final_winner?
     end
     display_goodbye
   end
